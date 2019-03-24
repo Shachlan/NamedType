674da6203c49c8f6361b8a3f8664a59300df08d6
AMS21 <hansfritzenmeister@yahoo.de>
2019-03-24 20:00:07 +0100

    Added BitWiseLeftShiftable

--- underlying_functionalities.hpp
+++ underlying_functionalities.hpp
@@ -149,6 +149,17 @@ struct BitWiseXorable : crtp<T, BitWiseXorable>
 };
 
 template <typename T>
+struct BitWiseLeftShiftable : crtp<T, BitWiseLeftShiftable>
+{
+    T  operator<<(T const& other) const { return T(this->underlying().get() << other.get()); }
+    T& operator<<=(T const& other)
+    {
+        this->underlying().get() <<= other.get();
+        return this->underlying();
+    }
+};
+
+template <typename T>
 struct Negatable : crtp<T, Negatable>
 {
     constexpr T operator-() const { return T(-this->underlying().get()); }
@@ -258,6 +269,7 @@ struct Arithmetic : Incrementable<T>,
                     BitWiseAndable<T>,
                     BitWiseOrable<T>,
                     BitWiseXorable<T>,
+                    BitWiseLeftShiftable<T>,
                     Negatable<T>,
                     Comparable<T>,
                     Printable<T>,
